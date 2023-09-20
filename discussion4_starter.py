203a3782c678f38c81d1645c81bf75710757e4e5
Dante Mitchell <dantemit@umich.edu>
2023-09-20 11:28:53 -0400

    third commit, im stuck

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -18,8 +18,8 @@ class Rectangle():
     #       "A rectangle with width ____ and height ____"
 
     # YOUR CODE HERE
-def __str__(self):
-    return f"A rectangle with width {self.width} and height {self.height}"
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
 
     # Create the "verify_input" method
@@ -29,11 +29,11 @@ def __str__(self):
     #       False otherwise
 
     # YOUR CODE HERE
-def verify_input(self, width, height):
-    if width > 0 and height > 0:
-        return True
-    else:
-        return False
+    def verify_input(width, height):
+        if width > 0 and height > 0:
+            return True
+        else:
+            return False
 
 
     # Create the "area" method
@@ -42,8 +42,12 @@ def verify_input(self, width, height):
     # Otherwise, it returns the area of the rectangle.
 
     # YOUR CODE HERE
-def area(width, height):
-    
+    def area(width, height):
+        if width <= 0 or height <= 0:
+            return "Invalid input"
+        area_1 = width * height
+        return area_1
+
 
 
     # Create the "perimeter" method
@@ -52,6 +56,11 @@ def area(width, height):
     # Otherwise, it returns the perimeter of the rectangle.
 
     # YOUR CODE HERE
+    def perimeter(width, height):
+        if width <= 0 or height <= 0:
+            return "Invalid input"
+        perimeter_1 = (2 * width) + (2 * height)
+        return perimeter_1
     
 
 
@@ -67,6 +76,8 @@ def main():
     print("Area:", r.area())
     print("Perimeter:", r.perimeter())
     print()
+main()
+
 
 if __name__ == "__main__":
     main()
